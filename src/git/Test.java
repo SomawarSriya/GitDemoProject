0ad8bebda2d7e32e5a76856b64582106e3e1b762
SriyaSomawar <SriyaSomawar@10.10.26.207>
2023-02-22 12:27:37 +0530

    First commit...

---
+++ src/git/Test.java
@@ -0,0 +1,16 @@
+package git;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+				
+		for(int i=1;i<=20;i+=2) {
+			System.out.println("The values are "+i);
+		}
+
+
+	}
+
+}
